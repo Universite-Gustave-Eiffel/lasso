279bf5b08617148709069ca7c97f13d597ad852e
Benoit Simard <contact@bsimard.com>
2022-09-09 18:09:51 +0200

    [client] trigger i18n action

--- packages/client/src/index.tsx
+++ packages/client/src/index.tsx
@@ -3,7 +3,6 @@ import { createRoot } from "react-dom/client";
 
 import reportWebVitals from "./reportWebVitals";
 import Root from "./views/Root";
-
 import "./styles/index.scss";
 
 const container = document.getElementById("root") as HTMLElement;
