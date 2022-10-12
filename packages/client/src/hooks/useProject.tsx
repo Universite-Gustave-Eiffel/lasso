06941161c93f88dbc6ce47c2a5a86960eb978eb5
Paul Girard <paul@ouestware.com>
2022-10-12 13:54:52 +0200

    [Client] eslint disabled + comment

--- packages/client/src/hooks/useProject.tsx
+++ packages/client/src/hooks/useProject.tsx
@@ -9,42 +9,47 @@ import { fromPairs, toPairs } from "lodash";
 export const useProject = (id: string): Project | null => {
   const [context, setContext] = useAppContext();
 
-  useEffect(() => {
-    /**
-     * Load all sources data from a project and store it in app context
-     * @param project
-     * @returns void
-     */
-    async function loadSources() {
-      const project = context.data.projects.find((e) => e.id === id);
-      if (project) {
-        const loadedProject = {
-          ...project,
-          sources: fromPairs(
-            await Promise.all(
-              toPairs(project.sources).map(async ([sourceId, source]) => {
-                if ("data" in source && typeof source.data === "string") {
-                  // fetch source.data
-                  const r = await axios.get<FeatureCollection>(source.data, { responseType: "json" });
-                  if (r.status === 200) return [sourceId, { ...source, data: r.data }];
-                  throw new Error(`Loading sources data ${sourceId} raised a ${r.status} HTTP code`);
-                }
-                return [sourceId, source];
-              }),
+  useEffect(
+    () => {
+      /**
+       * Load all sources data from a project and store it in app context
+       * @param project
+       * @returns void
+       */
+      async function loadSources() {
+        const project = context.data.projects.find((e) => e.id === id);
+        if (project) {
+          const loadedProject = {
+            ...project,
+            sources: fromPairs(
+              await Promise.all(
+                toPairs(project.sources).map(async ([sourceId, source]) => {
+                  if ("data" in source && typeof source.data === "string") {
+                    // fetch source.data
+                    const r = await axios.get<FeatureCollection>(source.data, { responseType: "json" });
+                    if (r.status === 200) return [sourceId, { ...source, data: r.data }];
+                    throw new Error(`Loading sources data ${sourceId} raised a ${r.status} HTTP code`);
+                  }
+                  return [sourceId, source];
+                }),
+              ),
             ),
-          ),
-        };
+          };
 
-        // putting back in context
-        setContext({
-          ...context,
-          data: { ...context.data, projects: [...context.data.projects.filter((e) => e.id !== id), loadedProject] },
-        });
+          // putting back in context
+          setContext({
+            ...context,
+            data: { ...context.data, projects: [...context.data.projects.filter((e) => e.id !== id), loadedProject] },
+          });
+        }
       }
-    }
 
-    loadSources();
-  }, [id]);
+      loadSources();
+    },
+    // context must not be a dep since we update it in the effect
+    // eslint-disable-next-line
+    [id, setContext],
+  );
 
   return context.data.projects.find((e) => e.id === id) || null;
 };
