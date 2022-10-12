56dc459f93ab277af648aaf02a3c0c95b60106ba
Paul Girard <paul@ouestware.com>
2022-10-12 18:20:58 +0200

    [Client] correct data cycle issue in useProject

--- packages/client/src/hooks/useProject.tsx
+++ packages/client/src/hooks/useProject.tsx
@@ -1,4 +1,4 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 import { Project } from "@lasso/dataprep";
 import { useAppContext } from "./useAppContext";
@@ -7,49 +7,43 @@ import { FeatureCollection } from "geojson";
 import { fromPairs, toPairs } from "lodash";
 
 export const useProject = (id: string): Project | null => {
-  const [context, setContext] = useAppContext();
+  const [context] = useAppContext();
+  const [project, setProject] = useState<Project | undefined>(context.data.projects.find((e) => e.id === id));
 
-  useEffect(
-    () => {
-      /**
-       * Load all sources data from a project and store it in app context
-       * @param project
-       * @returns void
-       */
-      async function loadSources() {
-        const project = context.data.projects.find((e) => e.id === id);
-        if (project) {
-          const loadedProject = {
-            ...project,
-            sources: fromPairs(
-              await Promise.all(
-                toPairs(project.sources).map(async ([sourceId, source]) => {
-                  if ("data" in source && typeof source.data === "string") {
-                    // fetch source.data
-                    const r = await axios.get<FeatureCollection>(source.data, { responseType: "json" });
-                    if (r.status === 200) return [sourceId, { ...source, data: r.data }];
-                    throw new Error(`Loading sources data ${sourceId} raised a ${r.status} HTTP code`);
-                  }
-                  return [sourceId, source];
-                }),
-              ),
+  useEffect(() => {
+    /**
+     * Load all sources data from a project and store it in app context
+     * @param project
+     * @returns void
+     */
+    async function loadSources() {
+      const projectToLoad = context.data.projects.find((e) => e.id === id);
+      if (projectToLoad) {
+        const loadedProject = {
+          ...projectToLoad,
+          sources: fromPairs(
+            await Promise.all(
+              toPairs(projectToLoad.sources).map(async ([sourceId, source]) => {
+                if ("data" in source && typeof source.data === "string") {
+                  // fetch source.data
+                  const r = await axios.get<FeatureCollection>(source.data, { responseType: "json" });
+                  if (r.status === 200) return [sourceId, { ...source, data: r.data }];
+                  throw new Error(`Loading sources data ${sourceId} raised a ${r.status} HTTP code`);
+                }
+                return [sourceId, source];
+              }),
             ),
-          };
+          ),
+        };
 
-          // putting back in context
-          setContext({
-            ...context,
-            data: { ...context.data, projects: [...context.data.projects.filter((e) => e.id !== id), loadedProject] },
-          });
-        }
+        // putting back in context
+        setProject(loadedProject);
       }
+    }
 
-      loadSources();
-    },
-    // context must not be a dep since we update it in the effect
-    // eslint-disable-next-line
-    [id, setContext],
-  );
+    loadSources();
+  }, [id, context.data.projects, setProject]);
+  //Suggestion add a useeffect Which add the change project back in the context if we were to use the context direclty in project map components.
 
-  return context.data.projects.find((e) => e.id === id) || null;
+  return project || null;
 };
