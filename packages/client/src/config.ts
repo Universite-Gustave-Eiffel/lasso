6d5e497e5fca61cd1fe3f6ff1cb70754daee1b55
Paul Girard <paul@ouestware.com>
2022-11-16 13:41:34 +0100

    [Client] limit highlighted to 3

--- packages/client/src/config.ts
+++ packages/client/src/config.ts
@@ -2,7 +2,7 @@ export const config = {
   data_path: `${process.env.PUBLIC_URL}/data`,
   debounceTime: 200,
   notificationTimeoutMs: 3000,
-  nbHiglightedProject: 4,
+  nbHiglightedProject: 3,
   defaultLocale: "en",
   transifexToken: process.env.TRANSIFEX_TOKEN || "1/78dd771fa7b374eeeae5827e6a2e44694dd1f925",
 };
