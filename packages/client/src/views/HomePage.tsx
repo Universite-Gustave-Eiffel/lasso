259479237faf1d8b9ca58533e76d04252e33aed2
Pierre Aumond <pierre.aumond@ifsttar.Fr>
2023-02-16 16:13:53 +0100

    change style

--- packages/client/src/views/HomePage.tsx
+++ packages/client/src/views/HomePage.tsx
@@ -14,7 +14,7 @@ export const HomePage: FC = () => {
   const higlightedProject = data.projects.slice(0, config.nbHiglightedProject);
 
   return (
-    <Layout heading={t("LASSO Plateform")}>
+    <Layout heading={t("Soundscape Mapping")}>
       <div className="row">
         <div className="col-12">
           <Markdown content={t("<center><br>Welcome to the LASSO platform. <br><br> This platform aims to highlight datasets related to the perception of sound environments and contrast them with standardized noise maps. <br> The datasets were collected in several projects described below.<br> <br> Enjoy your visit!</center>")} />
