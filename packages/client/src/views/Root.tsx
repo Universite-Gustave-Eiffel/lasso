d7b928acc2ca2c75ac961edfe48718c811fe6372
Benoit Simard <contact@bsimard.com>
2023-07-17 17:02:19 +0200

    [client] Hash router #10

--- packages/client/src/views/Root.tsx
+++ packages/client/src/views/Root.tsx
@@ -1,5 +1,5 @@
 import { FC } from "react";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 
 import { AppContextProvider } from "../core/context";
 import { Routing } from "../core/routing";
@@ -7,13 +7,13 @@ import { I18N } from "../core/i18n";
 
 const Root: FC = () => {
   return (
-    <BrowserRouter basename={process.env.PUBLIC_URL}>
+    <HashRouter basename={process.env.PUBLIC_URL}>
       <I18N>
         <AppContextProvider>
           <Routing />
         </AppContextProvider>
       </I18N>
-    </BrowserRouter>
+    </HashRouter>
   );
 };
 
