80ae56c2f44a0461b914bbfa941ab21606c997b4
Benoit Simard <contact@bsimard.com>
2023-07-18 12:01:32 +0200

    [client] Get default locale from browser/localstorage #14

--- packages/client/src/core/i18n.tsx
+++ packages/client/src/core/i18n.tsx
@@ -1,6 +1,6 @@
-import { FC, PropsWithChildren, useEffect } from "react";
+import { FC, PropsWithChildren, useCallback, useEffect } from "react";
 import { tx } from "@transifex/native";
-import { useTranslations, useTX } from "@transifex/react";
+import { useTranslations, useTX, useLanguages, useLocale } from "@transifex/react";
 
 import { config } from "../config";
 import { Loader } from "../components/Loader";
@@ -12,10 +12,48 @@ export const I18N: FC<PropsWithChildren> = ({ children }) => {
   tx.init({ token: config.transifexToken });
   const transifex = useTX();
   const { ready } = useTranslations();
+  const languages = useLanguages();
+  const locale = useLocale();
 
+  const getInitialLocal = useCallback(() => {
+    const availableLocales: string[] = languages.map((l: { code: string }) => l.code);
+
+    // taking locale strored in localstorage
+    const localeInLS = localStorage.getItem("locale");
+    if (localeInLS && availableLocales.includes(localeInLS)) {
+      return localeInLS;
+    }
+
+    // taking browser's local
+    if (navigator.languages) {
+      for (const locale of navigator.languages) {
+        if (availableLocales.includes(locale)) {
+          return locale;
+        }
+      }
+    }
+
+    // return default locale
+    return config.defaultLocale;
+  }, [languages]);
+
+  /**
+   * When transifex is ready
+   * => set the locale
+   */
   useEffect(() => {
-    if (ready) transifex.setCurrentLocale(config.defaultLocale);
-  }, [ready, transifex]);
+    if (ready) {
+      transifex.setCurrentLocale(getInitialLocal());
+    }
+  }, [ready, transifex, getInitialLocal]);
+
+  /**
+   * When locale changed
+   * => store it in the local storage
+   */
+  useEffect(() => {
+    if (locale) localStorage.setItem("locale", locale);
+  }, [ready, locale]);
 
   return (
     <>
